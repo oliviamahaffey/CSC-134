86f6f185557065aaa2ee91e92d018a16d89edb15
oliviamahaffey <mahaffeo1391@student.faytechcc.edu>
2025-01-15 16:16:42 +0000

    made hello world post

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+// CSC 134 
+// Hello World
+// oliviamahaffey
+// 1/15/2025
+
+#include <iostream>
+  
+using namespace std;
+  
+int main()
+{
+   cout << "Hello, World!" << endl;
+   cout << "I don't know what I'm doing!" << endl; 
+   return 0;
+}
