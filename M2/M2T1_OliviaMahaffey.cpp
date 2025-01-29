073862f62aaad5a3e39675f9f78069bcef5302e4
oliviamahaffey <mahaffeo1391@student.faytechcc.edu>
2025-01-29 16:02:49 +0000

    copied M2T1 code

--- M2/M2T1_OliviaMahaffey.cpp
+++ M2/M2T1_OliviaMahaffey.cpp
@@ -1,5 +1,5 @@
 // CSC 134
-// M1LAB - Apple Orchard
+// M2T1
 // Olivia Mahaffey
 // 1/29/2025
 
@@ -9,13 +9,25 @@ using namespace std;
 int main() {
    // This program will simulate an apple orchard.
    // The owner is Olivia Mahaffey
-string farm_name = " Liv's";
+string farm_name = " Liv's"
 int num_apples =  10;
 double cost_each = 0.25;
 
-cout << "Welcome to the" << farm_name  << " apple farm!" << endl;
+//new variables for M2T1
+
+string user_name; 
+int apples_to_buy;
+
+//greet customer
+cout << "Hello! What is your name? ";
+cin >> user_name; 
+cout << "Thanks for coming by, " << user_name << "!" << endl; 
+
+cout << "Welcome to the" << farm_name << " apple farm!" << endl;
 cout << "There are " << num_apples << " apples in stock." << endl;
 cout << "They cost $" << cost_each << " each." << endl;
+
+cout << setprecision(2) << fixed;
 
 // Find total price
 double total_cost = num_apples * cost_each;
@@ -24,5 +36,3 @@ cout << "The price for all of them is: $" << total_cost << endl;
 cout << endl; 
 return 0;
 }
-
-
