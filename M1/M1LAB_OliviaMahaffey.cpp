746d7ab0342defd05210f68012df94f11d3c81da
oliviamahaffey <mahaffeo1391@student.faytechcc.edu>
2025-01-29 15:39:36 +0000

    paste from textbook

---
+++ M1/M1LAB_OliviaMahaffey.cpp
@@ -0,0 +1,22 @@
+// CSC 134
+// M1LAB
+// Olivia Mahaffey
+// 1/29/2025
+
+#include <iostream>
+using namespace std;
+int main() {
+// This program will simulate an apple orchard.
+// The owner’s name
+string name = “Your Name”
+// number of apples owned
+int apples = 100
+// price per apple
+double pricePerApple = 0.25
+// calculate the total price of the apples
+// TODO
+// print all the information about the orchard
+// TODO
+cout << “Welcome to “ << name;
+cout << “ ’s apple orchard.” << endl;
+}
