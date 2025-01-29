184434d601ac333a28f4e8b0d83dccb71ce2d04e
oliviamahaffey <mahaffeo1391@student.faytechcc.edu>
2025-01-29 15:31:49 +0000

    hello student assignment

---
+++ M1T2_OliviaMahaffey.cpp
@@ -0,0 +1,17 @@
+// CSC 134
+// M1T2
+// Olivia Mahaffey
+// 1/29/2025
+
+#include <iostream>
+  
+using namespace std;
+  
+int main()
+{
+   cout << "Olivia Mahaffey" << endl;
+   cout << "University Transfer, C-STEP" << endl; 
+   cout << "I enjoy playing video games (FFVII!)" <<  endl;
+
+   return 0;
+}
