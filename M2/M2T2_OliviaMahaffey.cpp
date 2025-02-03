1b7eb5f6e17e372c8f2d10208f3b55f64690d488
oliviamahaffey <mahaffeo1391@student.faytechcc.edu>
2025-02-03 15:41:23 +0000

    started M2T2

---
+++ M2/M2T2_OliviaMahaffey.cpp
@@ -0,0 +1,33 @@
+// CSC 134
+// M2T2
+// Olivia Mahaffey
+// 02/03/2025
+
+// make a simple sales program that creates receipts
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+// set up variables 
+string store_name = "Liv Burgers"; 
+string food_item = "Cheeseburger";  
+double food_price = 5.99; 
+
+// take order
+
+cout << "Welcome to " << store_name << "!" << endl;
+cout << "You have one " << food_item << " that is $5.99." << endl; 
+
+// find totals
+
+// print receipt
+
+cout << "Have a nice day!" << endl; 
+cout << endl; 
+
+
+
+return 0;
+}
