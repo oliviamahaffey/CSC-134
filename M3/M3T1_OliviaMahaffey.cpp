1228a0ce97385981ae82f0929650a36cfeac6014
oliviamahaffey <mahaffeo1391@student.faytechcc.edu>
2025-02-23 22:41:58 +0000

    finished m3t1

---
+++ M3/M3T1_OliviaMahaffey.cpp
@@ -0,0 +1,52 @@
+// CSC 134
+// M3T1 
+// oliviamahaffey
+// 2/17/2025
+
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+    // Variables
+    double len1, wid1, len2, wid2; 
+    double area1, area2;
+
+    // Ask user for the length and width
+    cout << "What's the length of the first rectangle? ";
+    cin >> len1;
+    cout << "What's the width of the first rectangle? ";
+    cin >> wid1;
+    cout << "What's the length of the second rectangle? ";
+    cin >> len2;
+    cout << "What's the width of the second rectangle? ";
+    cin >> wid2;
+
+    // Find the area
+    area1 = len1 * wid1;
+    area2 = len2 * wid2; 
+
+    // Print the area
+    cout << "Rectangle One has an area of " << area1 << "." << endl; 
+    cout << "Rectangle Two has an area of " << area2 << "." <<endl; 
+
+    // Determine which is larger
+    if (area1 > area2) { 
+    cout << "Rectangle One is larger." << endl;
+    }
+
+    if (area2 > area1) {
+    cout << "Rectangle Two is larger." << endl;
+    }
+
+    if (area1 == area2) {
+    cout << "The rectangles are equal in size." << endl;
+    }
+    cout << endl; 
+    cout << "Calculction finished." << endl; 
+    cout << endl;
+
+    return 0;
+}
