334a995dfe39ab39212f7008e08ac02a70067495
oliviamahaffey <mahaffeo1391@student.faytechcc.edu>
2025-02-26 16:05:44 +0000

    m3hw q1

---
+++ M3/M3HW1_OliviaMahaffey.cpp
@@ -0,0 +1,41 @@
+// CSC-134
+// M3HW1 - ??
+// oliviamahaffey
+// 2/26/2025
+
+#include <iostream>
+using namespace std; 
+
+int main() {
+   // question 1
+   cout << "Question #1 -- Chatbot" << endl;  
+   cout << endl;
+   // code 
+   cout << "Welcome to the Chatbot!" << endl; 
+   cout << ". . . " << endl; 
+   cout << "Do you like sushi 🍣? Please type 'yes' or 'no'." << endl;
+   string choice;
+   string yes = "yes";
+   string no = "no";
+   cin >> choice; 
+   // if else
+   if (choice == yes){
+    cout << "Cool! I would say 'me too', but I am a robot." << endl;
+   }
+   if (choice == no){
+    cout << "The person programming me disagrees with you. Sorry!" << endl;
+   }
+   else {
+    cout << "I'm sorry, I didn't quite get that..." << endl; 
+   }
+
+
+
+
+
+
+
+
+
+    return 0;
+}
