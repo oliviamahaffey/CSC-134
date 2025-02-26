51376d59d2efcc8d60fda109f5fde5756f231256
oliviamahaffey <mahaffeo1391@student.faytechcc.edu>
2025-02-26 16:07:05 +0000

    spacing error

--- M3/M3HW1_OliviaMahaffey.cpp
+++ M3/M3HW1_OliviaMahaffey.cpp
@@ -21,12 +21,15 @@ int main() {
    // if else
    if (choice == yes){
     cout << "Cool! I would say 'me too', but I am a robot." << endl;
+    cout << endl;
    }
    if (choice == no){
     cout << "The person programming me disagrees with you. Sorry!" << endl;
+    cout << endl;
    }
    else {
     cout << "I'm sorry, I didn't quite get that..." << endl; 
+    cout << endl;
    }
 
 
