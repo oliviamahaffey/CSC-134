b761e0452a509f5cd5df76007c17cdabeb4bef9d
oliviamahaffey <mahaffeo1391@student.faytechcc.edu>
2025-03-26 15:30:50 +0000

    m4hw1 broken !

--- M4/M4HW1_OliviaMahaffey.cpp
+++ M4/M4HW1_OliviaMahaffey.cpp
@@ -5,27 +5,28 @@
 
 #include <iostream> 
 using namespace std; 
+void sum_min_multi (int sum, minimum, multiple);
 
 int main(){
-
 cout << "FIVE TIMES TABLE" << endl; 
 cout << "----------------" << endl; 
-
 const int MIN = 1; 
-    const int MAX = 12; 
-    int minimum = MIN; 
-    int maximum = MAX; 
-    int multiple, sum; 
-    multiple = 5; 
+const int MAX = 12; 
     while (minimum <= maximum) {
-        sum = minimum * multiple; 
+        int math = sum_min_multi(); 
         cout << multiple << " times " << minimum << " is " << sum << "." << endl; 
        // cout << "     " << minimum << "\t" << sum << endl; (makes a chart not what assignment wants :( )
         minimum = minimum + 1; 
     }
-
-
-
-
 return 0;
 }
+
+void sum_min_multi(){
+    int minimum = MIN; 
+    int maximum = MAX; 
+    int multiple, sum; 
+    multiple = 5; 
+    sum = minimum * multiple;
+
+
+} 
