df47d9859efa15ae58c6d129a4f31c9843947945
oliviamahaffey <mahaffeo1391@student.faytechcc.edu>
2025-04-21 15:02:38 +0000

    added vector code

--- M6/M6LAB1_OliviaMahaffey.cpp
+++ M6/M6LAB1_OliviaMahaffey.cpp
@@ -29,11 +29,27 @@ int main() {
         cout << "Next shell: " << shellname << endl; 
     }
 
+    // example 1
+    // .size() tels how many items are in a vector, .push_back() adds an item to the end of the vector, .pop_back() removes an item from the end
 
-
-
-
-
+    vector<int> mag; 
+    // load
+    mag.push_back(RED); 
+    mag.push_back(BLACK); 
+    int num_shells = 2; 
+    // look at shells
+    int current_shell = mag.at(num_shells-1); 
+    cout << "Next shell: " << current_shell << endl; 
+    // eject a shell
+    mag.pop_back(); 
+    num_shells--;
+    // repeat process
+    current_shell = mag.at(num_shells-1); 
+    cout << "Next shell: " << current_shell << endl; 
+    mag.pop_back(); 
+    cout << "Mag contains " << mag.size() << " shells" << endl; 
+    cout << "It could hold up to " << mag.capacity() << " before resizing." << endl; 
+ 
 
 
 
